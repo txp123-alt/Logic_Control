bd4e23c66237cde2e3064050e2f35f12d9ce031f
TxpGit <1101642762@qq.com>
2024-06-30 23:59:48 +0800

    主要逻辑控制项目第一次提交

---
+++ src/main/java/com/txp/logic_control/LogicControlApplication.java
@@ -0,0 +1,13 @@
+package com.txp.logic_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogicControlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogicControlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/txp/logic_control/LogicControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.txp.logic_control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogicControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
